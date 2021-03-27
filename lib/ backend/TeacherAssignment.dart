bbf37560feca43b69e65a62ab57d7f18b1a0790b
chrischongyj <chris.productions7@gmail.com>
2021-03-28 00:04:53 +0800

    Some changes

--- lib/ backend/TeacherAssignment.dart
+++ lib/ backend/TeacherAssignment.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
+//code here
+
 class TeacherAssignment {
   // CollectionReference users = FirebaseFirestore.instance.collection('users');
   String userId = FirebaseAuth.instance.currentUser.uid;
