98da2d36206b50cdaf168a8a45c461636d64e567
Smilawhile <smilawhile@users.noreply.github.com>
2021-04-01 20:28:42 +0800

    Changes

--- lib/Boundary_classes/PVP.dart
+++ lib/Boundary_classes/PVP.dart
@@ -1,73 +1,101 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'levelcreatorUI.dart';
+import 'package:ssadgame/Boundary_classes/levelcreatorUI.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(title: const Text('PVP'), centerTitle: true),
-          backgroundColor: Colors.white,
-          body: PVPage()),
-    ),
-  );
+// void main() {
+//   runApp(
+//     MaterialApp(
+//       home: Scaffold(
+//           appBar: AppBar(title: const Text('PVP'), centerTitle: true),
+//           backgroundColor: Colors.white,
+//           body: PVPage()),
+//     ),
+//   );
+// }
+
+class PVPage extends StatefulWidget {
+  @override
+  _PVPageState createState() => _PVPageState();
 }
 
-class PVPage extends StatelessWidget {
+class _PVPageState extends State<PVPage> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-        child: Column(
-      children: <Widget>[
-        SizedBox(height: 200.0),
-        Center(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => challengeUI()),
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                shape: new RoundedRectangleBorder(
-                  borderRadius: new BorderRadius.circular(30.0),
-                ),
-              ),
-              child: Row(children: <Widget>[
-                Icon(Icons.whatshot_rounded),
-                SizedBox(width: 20.0),
-                Text("Challenge"),
-              ]),
-            ),
+    return Scaffold(
+        resizeToAvoidBottomInset: true,
+
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('PVP'),
+
+          backgroundColor: Colors.white,
+          toolbarHeight: 60,
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back_ios_rounded, color: Colors.grey),
+            onPressed: () {
+              Navigator.popAndPushNamed(context, '/HomePage');
+            },
           ),
         ),
-        SizedBox(height: 30.0),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => levelCreator()),
-              );
-            },
-            style: ElevatedButton.styleFrom(
-              shape: new RoundedRectangleBorder(
-                borderRadius: new BorderRadius.circular(30.0),
-              ),
+
+        body: Column(
+          children: [
+            Container(
+              decoration: new BoxDecoration(color: Colors.white),
             ),
-            child: Row(children: <Widget>[
-              Icon(Icons.add_circle_outline_sharp),
-              SizedBox(width: 20.0),
-              Text("Create PVP levels"),
-            ]),
-          ),
-        )
-      ],
-    ));
+
+
+              SizedBox(height: 200.0),
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => challengeUI()),
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      shape: new RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(30.0),
+                      ),
+                    ),
+                    child: Row(children: <Widget>[
+                      Icon(Icons.whatshot_rounded),
+                      SizedBox(width: 20.0),
+                      Text("Challenge"),
+                    ]),
+                  ),
+                ),
+              ),
+              SizedBox(height: 30.0),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => levelCreator()),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    shape: new RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(30.0),
+                    ),
+                  ),
+                  child: Row(children: <Widget>[
+                    Icon(Icons.add_circle_outline_sharp),
+                    SizedBox(width: 20.0),
+                    Text("Create PVP levels"),
+                  ]),
+                ),
+              )
+            ],
+
+
+        ));
   }
 }
 
